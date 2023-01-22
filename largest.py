c2f27b9832e2a702dffda41427a41a56049b974b
Celardo <101440009+Celardo@users.noreply.github.com>
2023-01-22 13:40:54 +0800

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,15 @@
+def whichLargest(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        return num1
+    elif num2 > num1 and num2 > num3:
+        return num2
+    else:
+        return num3
+
+num1 = int(input("Please enter the first number: "))
+num2 = int(input("Please enter the second number: "))
+num3 = int(input("Please enter the third number: "))
+
+largest = whichLargest(num1, num2, num3)
+
+print (largest)
